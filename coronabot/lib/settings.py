f499eab81bc70944e1d155cfe41fa8f86c4e0854
dimagamera <note.dima.gamera@gmail.com>
2020-04-16 16:28:01 +0400

    Update 16.04.2020

--- coronabot/lib/settings.py
+++ coronabot/lib/settings.py
@@ -1,5 +1,5 @@
 ############## TELEGRAM BOT ########################
-token = ""
+token = "1113083009:AAEHdsPlQ38p6DO_pJ2d8zWNpVgSJFx7REI"
 ############## MSQL DATABASES ######################
 host = "localhost"
 user = "root"
--- coronabot/start.py
+++ coronabot/start.py
@@ -9,14 +9,25 @@ bot = telebot.TeleBot(token)
 # db_object.save_all_data(covid_19_data)
 
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_sticker(
+        message.chat.id, 'CAACAgIAAxkBAAJHnV6XFGmleFAuqbkOCpPyOb1AWAODAAILAANuM_gRBymXN2LhKucYBA')
+    bot.send_message(
+        message.chat.id, '🌏 Для використання бота, напишіть назву країни, \n🌏 Наприклад: Ukraine, Italy, China, Russian Federation')
+    bot.send_voice(message.chat.id, "http://d.zaix.ru/iK2U.mp3")
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     countr = message.text
     coron = db_object.show_country(countr)
     if message.text == countr:
         for item in coron:
-            bot.send_message(message.from_user.id, "Оперативна інформація про поширення коронавірусної інфекції 🌏 [𝐂𝐎𝐕𝐈𝐃-19] 🌏\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯"+"\n✈️ Країна ✈️ → " + str(item[1]) +
+            bot.send_message(message.from_user.id, "Оперативна інформація про поширення коронавірусної інфекції 🌏 [𝐂𝐎𝐕𝐈𝐃-19] 🌏\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯"+"\n✈️ Країна ✈️ → " + str(item[1]) +
                              "\n🤧 Кількість захворювань 🤧 → " + str(item[4]) + "\n🤧 Кількість захворювань за добу 🤧 → " + str(item[5]) + "\n☠️ Кількість смертей ☠️ → " + str(item[6]) + "\n☠️ Кількість смертей за добу ☠️ → " + str(item[5]) + "\n💊 Кількість вилікуваних 💊 → " + str(item[8]) + "\n💊 Кількість вилікуваних за добу 💊 → " + str(item[7]))
+    bot.send_sticker(
+        message.chat.id, "CAACAgIAAxkBAAJHrV6XFbrPmHvDNA2ynmPIGzpvUhU9AALOAQACVp29Cq2jmuzmnvpMGAQ")
 
 
 bot.polling(none_stop=True)
